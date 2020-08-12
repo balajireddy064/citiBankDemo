f759f4b73f4a7464c33ba42a2f5abe3db5c82475
breddy2 <breddy2@BREDDY2-F2CM7BG.lenovo.com>
2020-08-12 15:01:30 +0530

    entity class are changed

--- DemoCityBankByStsTools/src/main/java/com/example/demo/DemoEntity.java
+++ DemoCityBankByStsTools/src/main/java/com/example/demo/DemoEntity.java
@@ -4,5 +4,32 @@ public class DemoEntity {
 	
 	private int sno;
 	private String sname;
+	private String sadd;
+	private String mobileNumber;
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSadd() {
+		return sadd;
+	}
+	public void setSadd(String sadd) {
+		this.sadd = sadd;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	
 
 }
